7c89fba934d3f8d43e259518f28f19da19f85f3e
erixun <erik.sund.berg20@hotmail.se>
2021-10-17 15:05:36 +0200

    echo & provide identity

--- deploy.sh
+++ deploy.sh
@@ -6,16 +6,18 @@ set -e
 # build
 yarn run build
 
-# navigate into the build output directory
+echo "changing to the build output directory"
 cd dist
+
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git init
 
 # provide identity
 git config user.email "erik.sund.berg20@hotmail.se"
 git config user.name "Erik Sundberg"
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-git init
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com:erixun/my-website.git main:gh-pages
