5c2686f512aa8deca1c9453765e81988d355a1e8
Erik Sundberg <erik.sundberg@postnord.com>
2021-10-17 11:40:47 +0200

    remove default content & include main-view

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <main-view />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
+import MainView from "./views/Main.vue";
 
 @Options({
   components: {
-    HelloWorld,
+    MainView
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+body {
+  margin: 0;
+  min-height: 100vh;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
