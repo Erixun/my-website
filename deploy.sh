e788df020ecbb244951a1944f708780ae7e2fbcd
erixun <erik.sund.berg20@hotmail.se>
2021-10-17 15:15:34 +0200

    revert to master

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,8 @@ set -e
 # build
 yarn run build
 
-echo "changing to the build output directory"
+echo "-  Changing to the build output directory"
 cd dist
-
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -20,5 +19,5 @@ git config user.name "Erik Sundberg"
 
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:erixun/my-website.git main:gh-pages
+git push -f git@github.com:erixun/my-website.git master:gh-pages
 cd -
