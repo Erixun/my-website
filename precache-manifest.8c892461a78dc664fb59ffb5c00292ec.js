1accd8ff3ebb94e346956bd9f8b1a97c14bfb9c9
Erik Sundberg <erik.sundberg@postnord.com>
2021-10-17 14:14:23 +0200

    deploy

---
+++ precache-manifest.8c892461a78dc664fb59ffb5c00292ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2e6a4793f864f7cac81",
+    "url": "/my-website/css/app.309e0160.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/my-website/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "864e86357b69eadff4cf4c55f573c6e1",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "b2e6a4793f864f7cac81",
+    "url": "/my-website/js/app.a6eee34e.js"
+  },
+  {
+    "revision": "eda76a32f4229251e0a3",
+    "url": "/my-website/js/chunk-vendors.31623381.js"
+  },
+  {
+    "revision": "ecbd845df5a7594ecf7bca959798f235",
+    "url": "/my-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/my-website/robots.txt"
+  }
+]);
