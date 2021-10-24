368f2665af203a7d598c53e9e58072319cce64b5
erixun <erik.sund.berg20@hotmail.se>
2021-10-24 20:27:12 +0200

    implement darkmode & some styling

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,99 +1,186 @@
 <template>
   <!-- MOBILE FIRST -->
-  <header>
-    <h1>erixun.dev</h1>
-    <nav>collapsible nav?</nav>
-    <article class="light-switch" title="light-bulb" icon="💡">💡</article>
-  </header>
-  <main>
-    <section class="presentation">
-      <article>
-        <h2>Introduction</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, culpa
-          nobis, placeat maiores rem, eos quod illum aspernatur amet quidem
-          quae. Quisquam, nemo facere. Dolorem harum obcaecati odio nobis earum.
-        </p>
-      </article>
-      <aside>
-        <h2>A bit about me</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-          Perspiciatis, ex culpa facere natus voluptates ipsum voluptas, nostrum
-          nobis deleniti cupiditate odit, a sunt et? Assumenda suscipit
-          laboriosam expedita consequatur optio.
-        </p>
-      </aside>
-    </section>
-    <section class="skills">
-      <h2>My Powers 💪</h2>
-      <ul>
-        <li>Vue</li>
-        <li>TypeScript</li>
-        <li>ASP.NET</li>
-      </ul>
-    </section>
-    <section class="currently">
-      <h2>I am currently...</h2>
-      <article class="currently-learning">
-        <h3>Learning 🧐</h3>
+  <div class="container">
+    <header>
+      <section class="my-title">
+        <h1>erixun.me</h1>
+      </section>
+      <section class="navigation">
+        <nav>nav</nav>
+      </section>
+      <section class="light-switch">
+        <button
+          class="light-bulb off"
+          id="light-bulb"
+          title="light-bulb"
+          @click="toggleLightMode()"
+          icon="💡"
+          role="button"
+        >
+          💡
+        </button>
+      </section>
+    </header>
+    <main>
+      <section class="presentation">
+        <article>
+          <h2>Introduction</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae,
+            culpa nobis, placeat maiores rem, eos quod illum aspernatur amet
+            quidem quae. Quisquam, nemo facere. Dolorem harum obcaecati odio
+            nobis earum.
+          </p>
+        </article>
+        <aside>
+          <h2>A bit about me</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Perspiciatis, ex culpa facere natus voluptates ipsum voluptas,
+            nostrum nobis deleniti cupiditate odit, a sunt et? Assumenda
+            suscipit laboriosam expedita consequatur optio.
+          </p>
+        </aside>
+      </section>
+      <section class="powers">
+        <h2>My Powers <span emoji>💪</span></h2>
         <ul>
-          <li>Azure Functions</li>
-          <li>React</li>
-          <li>Clean Code</li>
+          <li>Vue</li>
+          <li>TypeScript</li>
+          <li>ASP.NET</li>
         </ul>
-      </article>
-      <article class="currently-workingon">
-        <h3>Working on 🛠️</h3>
-        <ul>
-          <li>A KanBan app</li>
-          <li>My own website</li>
-          <li>An Investment app</li>
-        </ul>
-      </article>
-    </section>
-  </main>
-  <footer>
-    contact info, made with, made by, hosted at, git repo, last updated
-  </footer>
+      </section>
+      <section class="currently">
+        <h2>I am currently...</h2>
+        <article class="currently-learning">
+          <h3>Learning <span emoji>🧐</span></h3>
+          <ul>
+            <li>Azure Functions</li>
+            <li>React</li>
+            <li>Clean Code</li>
+          </ul>
+        </article>
+        <article class="currently-workingon">
+          <h3>Working on <span emoji>🛠️</span></h3>
+          <ul>
+            <li>A KanBan app</li>
+            <li>My own website</li>
+            <li>An Investment app</li>
+          </ul>
+        </article>
+      </section>
+    </main>
+    <footer>
+      contact info, made with, made by, hosted at, git repo, last updated
+    </footer>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onMounted } from "vue";
 
 export default defineComponent({
   name: "main-view",
-  // setup() {
-  // },
+  setup() {
+    const hasLightsOff = "hasLightsOff";
+
+    const toggleLightMode = () => {
+      switchLightBulb();
+      switchLightMode();
+    };
+
+    const switchLightBulb = () => {
+      const lightBulb = document.getElementById("light-bulb");
+      lightBulb?.classList.toggle("off");
+      lightBulb?.blur();
+    };
+
+    const switchLightMode = () => {
+      const mainView = document.getElementById("main-view");
+      if (mainView) {
+        const classList = mainView.classList;
+        classList.toggle("lights-off");
+        storeLightMode(classList);
+      }
+    };
+
+    const storeLightMode = (classList: DOMTokenList) => {
+      localStorage.setItem(hasLightsOff, `${classList.contains("lights-off")}`);
+    };
+
+    onMounted(() => {
+      const prefersDark = localStorage.getItem(hasLightsOff);
+      if (prefersDark === "false") {
+        toggleLightMode();
+      }
+    });
+
+    return { toggleLightMode };
+  },
 });
 </script>
 
 <style lang="scss">
+:root {
+  --darkmode-default-bgc: #131516;
+  --darkmode-neutral-text: #d8d4cf;
+  --darkmode-default-text: rgb(174, 194, 211);
+  --darkmode-selection-background: #004daa;
+  --darkmode-selection-text: #e8e6e3;
+  --title-color: darkcyan;
+  --emphatic-color: #590000;
+  --fade-in-duration: 1.5s;
+}
+
+.lights-off {
+  color: var(--darkmode-default-text);
+  background-color: var(--darkmode-default-bgc);
+
+  [emoji] {
+    animation: emoji-fade-in var(--fade-in-duration) ease-out;
+  }
+}
+
 /* GLOBAL STYLING */
 body {
   margin: 0;
   min-height: 100vh;
+  #app {
+    min-height: inherit;
+  }
 }
 
-#app {
+.container {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   min-height: inherit;
   display: flex;
   flex-direction: column;
 }
+.container.lights-off {
+  transition: color var(--fade-in-duration) ease-out;
+}
+.container:not(.lights-off) {
+  color: unset;
+  background-color: unset;
+  h1 {
+    color: var(--title-color)
+  }
+}
 
-#app > * {
-  border: 1px solid grey;
+.container > * {
+  border-bottom: 1px solid;
+  box-sizing: border-box;
 }
 
 h1,
 h2,
-h3 {
+h3,
+button {
   all: unset;
+  text-transform: capitalize;
 }
 
 ul,
@@ -107,17 +194,45 @@ header {
   display: flex;
   padding: 1rem;
   align-items: baseline;
-  h1,
-  .light-switch {
+  justify-content: space-evenly;
+  section {
     flex-grow: 1;
+    h1 {
+      font-size: 1.1rem;
+      font-weight: bold;
+      font-style: italic;
+      text-transform: unset;
+    }
   }
-  nav {
-    flex-grow: 4;
-  }
-  .light-switch:not(.off) {
-    text-shadow: 0 0 10px orange;
-    position: relative;
-    font-size: 1rem;
+  .light-switch {
+    text-align: right;
+    .light-bulb {
+      cursor: pointer;
+      position: relative;
+      font-size: 1rem;
+      font-weight: bolder;
+      padding: 0 1rem;
+    }
+    .light-bulb.off {
+      color: transparent;
+      text-shadow: 0 0 10px #fff;
+      animation: bulb-fade-in var(--fade-in-duration) ease-out;
+    }
+    .light-bulb.off:focus {
+      text-shadow: 0 0 1px #fff;
+    }
+    .light-bulb.off::before {
+      content: attr(icon);
+      position: absolute;
+      text-shadow: 0 0 0 rgba(0, 0, 0, 0.5);
+    }
+    .light-bulb:not(.off) {
+      text-shadow: 0 0 10px orange;
+      filter: blur(0.6px) opacity(0.6);
+    }
+    .light-bulb:not(.off):focus {
+      filter: brightness(105%);
+    }
   }
 }
 
@@ -126,11 +241,14 @@ main {
   flex-grow: 8;
   display: flex;
   flex-direction: column;
+  margin-top: 1rem;
+  gap: 1rem;
   section {
-    border-bottom: 1px solid grey;
+    border-bottom: 1px solid;
+    flex-grow: 1;
+    display: flex;
   }
   .presentation {
-    flex-grow: 8;
     justify-content: space-evenly;
     display: flex;
     flex-wrap: wrap;
@@ -144,21 +262,49 @@ main {
       flex-basis: 200px;
     }
   }
+  .powers {
+    flex-direction: column;
+    justify-content: center;
+  }
 
   .currently {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-around;
+    row-gap: 1rem;
     h2 {
       width: 100%;
     }
   }
+}
+/*FOOTER STYLING */
+footer {
+  padding: 1rem;
+}
 
-  /* MEDIA QUERIES */
-  @media screen and (min-width: 700px) {
-    .presentation article {
-      order: 0;
-    }
+/* MEDIA QUERIES */
+@media screen and (min-width: 700px) {
+  main .presentation article {
+    order: 0;
+  }
+}
+
+/* KEYFRAMES */
+@keyframes emoji-fade-in {
+  0% {
+    color: transparent;
+  }
+  100% {
+    color: inherit;
+  }
+}
+
+@keyframes bulb-fade-in {
+  0% {
+    text-shadow: 0 0 0 transparent;
+  }
+  100% {
+    text-shadow: 0 0 10px #fff;
   }
 }
 </style>
