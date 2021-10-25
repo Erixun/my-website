0a9cfee07ce3e7fdeee1608b3ba0b7d1d761873a
erixun <erik.sund.berg20@hotmail.se>
2021-10-25 14:19:46 +0200

    import TheLightBulb

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -9,16 +9,7 @@
         <nav>nav</nav>
       </section>
       <section class="light-switch">
-        <button
-          class="light-bulb off"
-          id="light-bulb"
-          title="light-bulb"
-          @click="toggleLightMode()"
-          icon="💡"
-          role="button"
-        >
-          💡
-        </button>
+        <TheLightBulb />
       </section>
     </header>
     <main>
@@ -77,46 +68,14 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted } from "vue";
+import { defineComponent } from "vue";
+import TheLightBulb from '@/components/TheLightBulb.vue'
 
 export default defineComponent({
   name: "main-view",
-  setup() {
-    const hasLightsOff = "hasLightsOff";
-
-    const toggleLightMode = () => {
-      switchLightBulb();
-      switchLightMode();
-    };
-
-    const switchLightBulb = () => {
-      const lightBulb = document.getElementById("light-bulb");
-      lightBulb?.classList.toggle("off");
-      lightBulb?.blur();
-    };
-
-    const switchLightMode = () => {
-      const mainView = document.getElementById("main-view");
-      if (mainView) {
-        const classList = mainView.classList;
-        classList.toggle("lights-off");
-        storeLightMode(classList);
-      }
-    };
-
-    const storeLightMode = (classList: DOMTokenList) => {
-      localStorage.setItem(hasLightsOff, `${classList.contains("lights-off")}`);
-    };
-
-    onMounted(() => {
-      const prefersDark = localStorage.getItem(hasLightsOff);
-      if (prefersDark === "false") {
-        toggleLightMode();
-      }
-    });
-
-    return { toggleLightMode };
-  },
+  components: { TheLightBulb },
+  // setup() {
+  // },
 });
 </script>
 
