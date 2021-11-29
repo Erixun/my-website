f1b164c2c831e5bb38553b64d5e112c32a75e1b0
erixun <erik.sund.berg20@hotmail.se>
2021-11-29 19:34:18 +0100

    made favorites section w fav podcasts

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -197,6 +197,89 @@
         </article>
       </div>
     </section>
+    <section class="main-section favorites flx-col">
+      <div class="md-container">
+
+      <h2>A Few Favorites</h2>
+      </div>
+      <div class="md-container">
+        <h3>Dev Podcasts</h3>
+        <ul class="ltext">
+          <li>
+            <article class="podcast">
+              <a href="https://podcasts.google.com/feed/aHR0cHM6Ly93d3cuY29kaW5nYmxvY2tzLm5ldC9wb2RjYXN0LWZlZWQueG1s" class="podcast-link">
+                <figure>
+                  <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRHZlxmt4pe3kZtc6PzMk2sXPtXngEI9zZ5WqEuyE4j4YgugZY" alt="Coding Blocks podcast logo" />
+                  <figcaption>
+                    <h4>Coding Blocks</h4>
+                    <small>A. Underwood, M. Outlaw, J. Zack</small>
+                  </figcaption>
+                </figure>
+              </a>
+            </article>
+          </li>
+          <li>
+            <article class="podcast">
+              <a href="https://podcasts.google.com/feed/aHR0cHM6Ly9mZWVkLnN5bnRheC5mbS9yc3M" class="podcast-link">
+                <figure>
+                  <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcT_DOr_E0YhD_4q5arVX4MVp4bC87IfEh50gmS037HK-WyaGISv" alt="Syntax podcast logo" />
+                  <figcaption>
+                    <h4>Syntax</h4>
+                    <small>Wes Bos & Scott Tolinski</small>
+                  </figcaption>
+                </figure>
+              </a>
+            </article>
+          </li>
+          <li>
+            <article class="podcast">
+              <a href="https://podcasts.google.com/feed/aHR0cHM6Ly9zaG9wdGFsa3Nob3cuY29tL2ZlZWQvcG9kY2FzdA" class="podcast-link">
+                <figure>
+                  <img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTRiTaHLEGNpQd1YJGWKcZpH3JYjkrOROO303Tz6krK0nPkO93C" alt="ShopTalk podcast logo"/>
+                  <figcaption>
+                    <h4>ShopTalk</h4>
+                    <small>ShopTalk</small>
+                  </figcaption>
+                </figure>
+              </a>
+            </article>
+          </li>
+          <li>
+            <article class="podcast">
+              <a
+                href="https://podcasts.google.com/feed/aHR0cHM6Ly9mZWVkcy5mZWVkd3JlbmNoLmNvbS9qcy1qYWJiZXIucnNz"
+                target="_blank"
+                class="podcast-link"
+              >
+                <figure>
+                  <img
+                    src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQmnN40h7oelbXM-GRCUZD7Zx4yio3nV5r2zdIMgNis9R5K7HE"
+                    alt="JavaScript Jabber podcast logo"
+                  />
+                  <figcaption>
+                    <h4>JavaScript Jabber</h4>
+                    <small>Top End Devs</small>
+                  </figcaption>
+                </figure>
+              </a>
+            </article>
+          </li>
+          <li>
+            <article class="podcast">
+              <a href="https://podcasts.google.com/feed/aHR0cHM6Ly90aGVkb3RuZXRjb3JlcG9kY2FzdC5saWJzeW4uY29tL3Jzcw" class="podcast-link">
+                <figure>
+                  <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTsmngZPTEBidXSe2PZr_zQ-LI9XFJa50claNHxy9DZM8sjQO0q" alt="The .NET Core Podcast logo" />
+                  <figcaption>
+                    <h4>The .NET Core Podcast</h4>
+                    <small>Jamie Taylor</small>
+                  </figcaption>
+                </figure>
+              </a>
+            </article>
+          </li>
+        </ul>
+      </div>
+    </section>
   </main>
   <footer>
     <div class="lg-container grid">
@@ -792,6 +875,56 @@ header.primary {
     }
     figcaption {
       margin-top: 2rem;
+    }
+  }
+
+  .favorites {
+    gap: 3vmax;
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-evenly;
+      gap: 0.5rem 2rem;
+      .podcast {
+        width: clamp(110px, 11vw, 180px);
+      }
+    }
+    .podcast * {
+      display: block;
+      text-decoration: none;
+      color: inherit;
+    }
+    figure {
+      margin: 0;
+      img {
+        width: 100%;
+        border-radius: 0.5rem;
+        box-shadow: 1px 1px 1px 1px #80808075;
+      }
+      h4 {
+        font-size: clamp(16px, 1vw, 20px);
+        margin: 7% 0 5%;
+        line-height: 1;
+      }
+      small {
+        font-size: 0.8rem;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+    }
+    .podcast-link {
+      padding: 0.5rem;
+      padding-bottom: 0.5rem;
+      border-radius: 0.8rem;
+      padding-bottom: 1rem;
+      &:hover {
+        background: #8080801c;
+      }
+      &:active {
+        color: var(--accented-text-color);
+        outline: 2px solid;
+      }
     }
   }
 }
